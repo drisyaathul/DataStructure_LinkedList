e6836ed4a306c9f15041c83e7a95c87e68e0c6cc
drisyaathul <drisyak809@gmail.com>
2023-08-06 21:55:07 +0800

    Added: Creating Simple LinkedList of 56,30,70

--- src/com/linkedlistdatastructure/LinkedListMain.java
+++ src/com/linkedlistdatastructure/LinkedListMain.java
@@ -1,7 +1,14 @@
 package com.linkedlistdatastructure;
 
+import java.util.LinkedList;
+
 public class LinkedListMain {
     public static void main(String[] args) {
-
+        //UC1: Create Simple Linked List of 56,30,70.
+        LinkedList<Integer> linkedList = new LinkedList<>();
+        linkedList.add(56);
+        linkedList.add(30);
+        linkedList.add(70);
+        System.out.println("Simple Linked List => "+linkedList);
     }
 }
